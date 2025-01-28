93340871a07a4d51c7e108d883279f8457eb9ed0
Laio Holanda <157860913+laioholanda@users.noreply.github.com>
2025-01-28 16:55:49 -0300

    Add files via upload

---
+++ swiper/src/css/javascript/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    effect: 'flip',
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
